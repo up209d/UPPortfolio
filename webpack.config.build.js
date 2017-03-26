fec327865691247ad7f397914c8c917388672d4c
up209d <up209d@gmail.com>
2017-03-26 11:47:34 +1100

    Dist to gh-pages

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -3,42 +3,79 @@ var webpack = require('webpack');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 
 var host = require("./host.config");
-var hostLocalAccess = "http://" + host.hostName+":"+host.hostPort;
-var hostNetworkAccess = "http://" + host.hostLanIP+":"+host.hostPort;
+var hostLocalAccess = "http://" + host.hostName + ":" + host.hostPort;
+var hostNetworkAccess = "http://" + host.hostLanIP + ":" + host.hostPort;
 
-process.env.NODE_ENV =  'development';
+process.env.NODE_ENV = 'production';
 
 module.exports = {
-    performance: { hints: false },
-    devtool: 'source-map',
-    externals: {
-
-    },
-    entry: [
-        './src/js/index.js',
-        'webpack-hot-middleware/client?reaload=true'
+  performance: {hints: false},
+  devtool: 'source-map',
+  externals: {},
+  entry: {
+    appBundle: [
+      './src/js/index.js'
     ],
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),new HTMLWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        })
-    ],
-    module: {
-        loaders: [
-            {test: /\.jsx?$/, include: path.join(__dirname, 'src'), loaders: ['react-hot-loader','babel-loader'], exclude: [/node_modules/]},
-            {test: /(\.css)$/, loaders: ['style-loader', 'css-loader'], exclude: [/node_modules/]},
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file', exclude: [/node_modules/]},
-            {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000', exclude: [/node_modules/]},
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream', exclude: [/node_modules/]},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml', exclude: [/node_modules/]}
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
-        publicPath: hostNetworkAccess + '/',
-        filename: '[name].js'
-    }
+    vendorBundler: [
+      'trianglify',
+      'pixi.js',
+      'gsap',
+      'snapsvg'
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      test: /\.jsx?$/,
+      comment: false,
+      compress: true,
+      mangle: true,
+      sourceMap: false,
+      // exclude: [/node_modules/,/bower_components/]
+    }),
+  ],
+  module: {
+    loaders: [
+      // Fix SnapSVG Import Issue
+      {
+        test: require.resolve('snapsvg'),
+        loader: 'imports-loader?this=>window,fix=>module.exports=0'
+      },
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, 'src'),
+        loaders: ['react-hot-loader', 'babel-loader'],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.(css|scss)$/,
+        loaders: ['style-loader', 'css-loader?sourceMap', "sass-loader?sourceMap"],
+        exclude: [/node_modules/]
+      },
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader', exclude: [/node_modules/]},
+      {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000', exclude: [/node_modules/]},
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+        exclude: [/node_modules/]
+      },
+      {test: /\.png$|\.jpe?g$|\.gif$/, loader: 'file-loader', exclude: [/node_modules/]},
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: hostNetworkAccess + '/',
+    filename: '[name].js'
+  }
 
 };
